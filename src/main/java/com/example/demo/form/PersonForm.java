bce0bb5df988b0fc9d82a9e9f73454089a03bdda
amittiwari19101989 <master.amittiwari@gmail.com>
2018-12-31 20:46:22 +0530

    lets see

---
+++ src/main/java/com/example/demo/form/PersonForm.java
@@ -0,0 +1,18 @@
+package com.example.demo.form;
+
+public class PersonForm {
+private String fname;
+public String getFname() {
+	return fname;
+}
+public void setFname(String fname) {
+	this.fname = fname;
+}
+public String getLname() {
+	return lname;
+}
+public void setLname(String lname) {
+	this.lname = lname;
+}
+private String lname;
+}
